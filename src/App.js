489032c0171ba703cb86cc56340c1706d7598718
Ishan Rathore <rathore.ishan@gmail.com>
2024-02-02 11:17:23 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './style.css';
+import logo from './logo.png';
 
 function App() {
+  const [userInput, setUserInput] = useState('');
+  const [aiResponse, setAiResponse] = useState('');
+
+  const handleButtonClick = async () => {
+
+    const newQuestion = userInput;
+
+    
+    const response = await fetch(
+      'https://api-inference.huggingface.co/models/mistralai/Mixtral-8x7B-Instruct-v0.1',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer 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',
+        },
+        body: JSON.stringify({ inputs: newQuestion }),
+      }
+    );
+
+    const data = await response.json();
+    console.log(data)
+    const aiReply = data[0].generated_text; 
+
+
+    setAiResponse(aiReply);
+    console.log(aiReply)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+        <h1>WELCOME TO FREE GPT!</h1>
+        <p>Developed by Ishan Rathore</p>
+        <img className='logo' src={logo} alt="" />
+        {aiResponse && (
+          <div className="ai-response">
+            <p><strong>FreeGPT:</strong> {aiResponse}</p>
+          </div>
+        )}
+        <div className='input'>
+        <div className='textarea-container'>
+          <textarea
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+            placeholder='Type your question here....'
+            rows="3"
+            cols="100"
+          ></textarea>
+          <button type="button" onClick={handleButtonClick}>
+            
+            
+          </button>
+        </div>
+      </div>
+      </div>
   );
 }
 
